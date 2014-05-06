725d98eb80eb9d504cb467b8020f64211b898176
Ethan Petuchowski <etahnp@gmail.com>
2014-05-05 20:21:18 -0700

    finish plot 3

--- plot3.R
+++ plot3.R
@@ -9,10 +9,32 @@ power_consumption <- '/Users/ethan/code/non_apple/data/household_power_consumpti
 
 # initial rough estimate
 plot3 <- function (data_loc = power_consumption) {
-    data <- read.csv(data_loc)
+
+    # Read raw data
+    data <- read.csv(data_loc, sep = ';', na.strings = c('?'),
+                     colClasses = c(rep('character', 2), rep('numeric', 7)),
+                     comment.char = '', nrows = 69518)
     
-    # example of overlaid scatterplots from lecture
-    plot(x, y, type = 'n')
-    lines(x[g == 'male'], y[g == 'male'], col = 'green')
-    lines(x[g == 'female'], y[g == 'female'], col = 'blue')
+    # only plot date range we care about
+    filtered <- subset(data, Date == '1/2/2007' | Date == '2/2/2007')
+    
+    png('plot3.png') # open PNG device
+    
+    # construct base line graph
+    plot(filtered$Sub_metering_1, col = 'black', type = 'l',
+         ylab = 'Energy sub metering', xlab = '', xaxt = 'n')
+    
+    # overlay other line graphs
+    lines(filtered$Sub_metering_2, col = 'red', type = 'l')
+    lines(filtered$Sub_metering_3, col = 'blue', type = 'l')
+    len <- length(filtered$Global_active_power)
+    
+    # add x-axis
+    axis(side = 1, at = c(1, len/2, len), labels = c('Thu', 'Fri', 'Sat'))
+    
+    # add caption
+    legend('topright', c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'),
+           col = c('black', 'red', 'blue'), lty = c(1,1,1))
+    
+    dev.off() # save PNG to filesystem
 }
