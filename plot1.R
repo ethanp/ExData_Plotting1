0295e5a1079d747b45d35732761c40d4da19f9f3
Ethan Petuchowski <etahnp@gmail.com>
2014-05-05 17:43:39 -0700

    plot1 seems done

--- plot1.R
+++ plot1.R
@@ -5,27 +5,31 @@
 # 5/5/14
 # Ethan Petuchowski
 
+setwd('/Users/ethan/code/non_apple/coursera/exploratory-data-analysis/ExData_Plotting1')
 power_consumption <- '/Users/ethan/code/non_apple/data/household_power_consumption.txt'
 
 plot1 <- function (data_loc = power_consumption) {
                                                   
-    # set column classes for faster reading
-    cl <- c(rep('character', 2), rep('numeric', 7))
+    # Read raw data
+    data <- read.csv(data_loc, 
+                     sep = ';', 
+                     na.strings = c('?'),  # save '?' in raw data as 'NA' value
+                     colClasses = c(rep('character', 2), rep('numeric', 7)),
+                     comment.char = '', 
+                     nrows = 69518)        # only read data we need
+
     
-    # nrows: only read up through the data we actually need
-    #        this speeds everything up tremendously
-    # na.strings: I checked and this is working properly
-    data <- read.csv(data_loc, sep=';', na.strings = c('?'), 
-                   colClasses = cl, comment.char = '', nrows = 69518)
+    # only plot date range we care about
+    filtered <- subset(data, Date == '1/2/2007' | Date == '2/2/2007')
+
     
-    # add column of Date datatype which contains both
-    # the correct date and time of each datapoint
-#    data$datetime <- apply(data, 1, function(row)
-#        strptime(paste(row[1], row[2]), format='%d/%m/%Y %H:%M:%S'))
+    # construct and save histogram
+    png('plot1.png')
     
-    plot <- hist(data$Global_active_power, col = 'red',
+    hist(filtered$Global_active_power,
+         col  = 'red',
          main = 'Global Active Power',
          xlab = 'Global Active Power (kilowatts)')
-
-    # TODO code that creates the PNG file, otw it looks done
+    
+    dev.off()
 }
--- plot2.R
+++ plot2.R
@@ -7,8 +7,31 @@
 
 power_consumption <- '/Users/ethan/code/non_apple/data/household_power_consumption.txt'
 
-# initial rough estimate
 plot2 <- function (data_loc = power_consumption) {
-    data <- read.csv(data_loc)
-    line(time, gap, data = data)
+    
+    # Read raw data
+    data <- read.csv(data_loc, 
+                     sep = ';', 
+                     na.strings = c('?'),  # save '?' in raw data as 'NA' value
+                     colClasses = c(rep('character', 2), rep('numeric', 7)),
+                     comment.char = '', 
+                     nrows = 69518)        # only read data we need
+    
+    # only plot date range we care about
+    filtered <- subset(data, Date == '1/2/2007' | Date == '2/2/2007')
+    
+    # add column of Date datatype which contains both
+    # the correct date and time of each datapoint
+    filtered$datetime <- apply(filtered, 1, function(row)
+        strptime(paste(row[1], row[2]), format='%d/%m/%Y %H:%M:%S'))
+    
+
+    
+    # construct histogram
+    plot <- plot(filtered$datetime, filtered$Global_active_power,
+                 col  = 'red',
+                 main = 'Global Active Power',
+                 xlab = 'Global Active Power (kilowatts)')
+    
+    # TODO code that creates the PNG file, otw it looks done
 }
